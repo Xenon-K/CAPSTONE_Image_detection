514da1828dd3b27e2294b9f82f83d417b1a2107d
Kabire-A <160642553+Kabire-A@users.noreply.github.com>
2025-03-14 15:03:25 -0700

    Update YOLO
    Feature - remap old yolo class index to current coco class index

--- YOLO Results creator/yolo_results_creator.py
+++ YOLO Results creator/yolo_results_creator.py
@@ -13,15 +13,16 @@ import os
 
 confidence_threshold = 0.3
 iou_threshold=0.45
+batch_size = 250
+convert_flag = True # if output is coco 80 true, if coco 91 false
 
-
-def process_h5(filename, confidence_threshold, category_mapping, file_no):
+def process_h5(filename, confidence_threshold, category_mapping, file_no, idx_convert):
     predictions = []
 
     with h5py.File(filename, "r") as file:
         # Get the logits and bboxes from the h5 file
         img_count = len(file['data']['0'])
-        image_id = 0 + (250 * file_no)  # get index shift based on batch no for accessing image detail csv
+        image_id = 0 + (batch_size * file_no)  # get index shift based on batch no for accessing image detail csv
         for i in range(img_count):
             # get original image id and dimensions from csv
             image_name = image_details[image_id]['Image Name']
@@ -33,54 +34,48 @@ def process_h5(filename, confidence_threshold, category_mapping, file_no):
             bboxes = torch.tensor(np.array((file['data']['0'][batch])))
             scores = torch.tensor(np.array((file['data']['1'][batch])))
             classes = torch.tensor(np.array((file['data']['2'][batch])))
-            bboxes = bboxes.squeeze(0)  # Shape (8400, 4)
-            scores = scores.squeeze(0)  # Shape (8400,)
-            classes = classes.squeeze(0)
-            # Define the NMS threshold
 
-            # Apply NMS
+            # fix shape for input into nms
+            bboxes = bboxes.squeeze(0)
+            scores = scores.squeeze(0)
+            classes = classes.squeeze(0)
+
+            # Apply nms
             indices = nms(bboxes, scores, iou_threshold)
 
-            # Use the indices to extract the remaining boxes, scores, and classes
-            bboxes = bboxes[indices]  # Shape (N, 4)
+            # Use the indices to extract the remaining boxes, scores, and classes, re-add batch dim
+            bboxes = bboxes[indices]
             bboxes = bboxes.unsqueeze(0)
-            scores = scores[indices]  # Shape (N,)
+            scores = scores[indices]
             scores = scores.unsqueeze(0)
             classes = classes[indices]
             classes = classes.unsqueeze(0)
-            #batch_args = [arg[nms_indices] for arg in batch_args]
+
             # check each prediction
             for j in range(len(scores[0])):
                 score = scores[0][j]
 
                 # if prediction meets confidence threshold get data and save to put in results json
                 if score.max() > confidence_threshold:
-                    if image_name == 724:
-                        print("hello")
+                    # get predicted class
                     pred_class=classes[0][j].item()
+                    if convert_flag:
+                        pred_class = idx_convert[pred_class]['coco_class']
                     #print("Class ", pred_class)
                     pred_score = score.item()  # Max confidence score
                     #print("Score ", pred_score)
                     pred_box = bboxes[0][j].tolist()  # The bounding box
 
-                    # Convert box from cxcywh format (detr) to xywh (coco)
+                    # Convert box from xyxy format (yolo) to xywh (coco)
                     pred_box = box_convert(torch.tensor(pred_box), 'xyxy', 'xywh').tolist()
 
                     # After converting to xywh, scale to original dimensions
                     xscale=image_width/640
                     yscale=image_height/640
-                    pred_box[0] = round(pred_box[0] * xscale, 2)  # x1 * image_width
-                    pred_box[1] = round(pred_box[1] * yscale, 2)  # y1 * image_height
-                    pred_box[2] = round(pred_box[2] * xscale, 2)  # x2 * image_width
-                    pred_box[3] = round(pred_box[3] * yscale, 2)  # y2 * image_height
-
-                    '''x_scale = image_width
-                    y_scale = image_height
-
-                    pred_box[0] = round(((pred_box[0] - (pred_box[2]/2)) * x_scale), 2)  # x1 * image_width
-                    pred_box[1] = round(((pred_box[1] - (pred_box[3]/2)) * y_scale), 2)  # y1 * image_height
-                    pred_box[2] = round(pred_box[2] * x_scale, 2)  # x2 * image_width
-                    pred_box[3] = round(pred_box[3] * y_scale, 2)  # y2 * image_height'''
+                    pred_box[0] = round(pred_box[0] * xscale, 2)  # xmin * xscale
+                    pred_box[1] = round(pred_box[1] * yscale, 2)  # ymin * yscale
+                    pred_box[2] = round(pred_box[2] * xscale, 2)  # w * xscale
+                    pred_box[3] = round(pred_box[3] * yscale, 2)  # h * yscale
 
                     # Store the result in list to store in json later
                     if category_mapping.get(pred_class, 0) != 0:
@@ -97,6 +92,8 @@ def process_h5(filename, confidence_threshold, category_mapping, file_no):
 # read the image details csv to get original image id and dimensions
 df = pd.read_csv('image_details.csv')
 image_details = df.to_dict(orient='index')
+df2 = pd.read_csv('old2newcoco.csv')
+index_convert = df2.to_dict(orient='index')
 
 # Read the class labels for coco
 with open("coco-labels-2014_2017.txt", "r") as f:
@@ -116,7 +113,7 @@ for h5_file in h5_files:
     try:
         # Read data from each h5 file and get results
         print("reading file: " + h5_file)
-        file_predictions = process_h5(h5_file_path, confidence_threshold, category_mapping, file_count)
+        file_predictions = process_h5(h5_file_path, confidence_threshold, category_mapping, file_count, index_convert)
         all_results.extend(file_predictions)
         file_count += 1
     except Exception as e:
