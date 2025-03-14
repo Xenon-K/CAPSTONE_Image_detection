9cda09268deece97af5695a0a072a3df11838694
Kabire-A <160642553+Kabire-A@users.noreply.github.com>
2025-03-14 15:25:04 -0700

    Update Cocoeval
    Feature - now saves results to a csv

--- cocoeval.py
+++ cocoeval.py
@@ -1,6 +1,7 @@
 import json
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
+import csv
 
 gt_file = 'results/instances_val2017.json'   # Ground truth annotations
 results_file = 'results/results.json'  # Predicted results
@@ -18,3 +19,32 @@ coco_eval = COCOeval(coco_gt, coco_dt, 'bbox')
 coco_eval.evaluate()
 coco_eval.accumulate()
 coco_eval.summarize()
+
+# get metrics in a variable
+metrics = coco_eval.stats
+
+# get data ready for csv
+header = ['Metric', 'Value']
+data = [
+    ('AP', round(metrics[0], 3)),
+    ('AP@.5', round(metrics[1], 3)),
+    ('AP@.75', round(metrics[2], 3)),
+    ('AP small', round(metrics[3], 3)),
+    ('AP medium', round(metrics[4], 3)),
+    ('AP large', round(metrics[5], 3)),
+    ('AR', round(metrics[6], 3)),
+    ('AR@.5', round(metrics[7], 3)),
+    ('AR@.75', round(metrics[8], 3)),
+    ('AR small', round(metrics[9], 3)),
+    ('AR medium', round(metrics[10], 3)),
+    ('AR large', round(metrics[11], 3))
+]
+
+# create results csv
+csv_filename = 'results/coco_results.csv'
+with open(csv_filename, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+    writer.writerows(data)
+
+print(f"COCO evaluation results saved to " + csv_filename)
