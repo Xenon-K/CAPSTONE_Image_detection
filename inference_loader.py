169d505cc20e0871e1f3088e5055dff85d5be2b3
Kabire-A <160642553+Kabire-A@users.noreply.github.com>
2025-03-14 21:03:59 -0700

    Update inference_loader.py
    update with group datasets

--- inference_loader.py
+++ inference_loader.py
@@ -3,23 +3,25 @@ from PIL import Image
 import qai_hub as hub
 
 # gets last 10 datasets and prints ids/summaries
-#dataset = hub.get_datasets(limit=10)
+#dataset = hub.get_datasets(limit=30)
 #print("Dataset information:", dataset)
 
 # detr coco dataset (1, 480,480, 3) float 32
-coco = ['d67ooy5n7', 'd67jod6n9', 'd67801862', 'd693pyql7', 'dr2qzxjl2',
-        'dq9koxj57','d693py1l7', 'dn7xx1v57', 'd67801op2', 'dp7lop8j7']
+coco = ['dp7035le7', 'd67oog6q7', 'dk7g4en07', 'dv91dn189', 'dv955r3m9',
+        'dp7lomyw7','dq9ko0w57', 'dj7dmlk87', 'd693pkwl7', 'dr2qzo362']
 
 # yolo (1, 640, 640, 3) float 32
-coco2 = ['dd9po5ww2', 'do7mox1d9', 'dw9vekn07', 'dv955ngg9', 'do7morzd9',
-        'dw26y0ke9','dd9poxgw2', 'dd9poxnn2', 'dp703xql7', 'd67jo1gn9']
+coco2 = ['dz2roj5g2', 'dn7xxkw67', 'd693pkq07', 'dq9ko0pd7', 'dj7dmljq7',
+        'dk7g4eje7','dn7xxkve7', 'dv91dn4n9', 'dw26yljz9', 'dj7dmlxk7',
+         'd6780vxl2','dp7lomm47', 'dn7xxkke7', 'd6780vvl2', 'dp7ld1142',
+         'dz7zzoo57','d09ywyy17', 'dv91ojjn2', 'd67oqkz69', 'dn7x4y5e2']
 
-model = hub.get_model("mqpdrkpoq")
+model = hub.get_model("mq8z0xxvq")
 device = hub.Device("Samsung Galaxy S24 Ultra")
 
 for i in coco2:
     dataset = hub.get_dataset(i)
-    # print("Dataset information:", dataset)
+    #print("Dataset information:", dataset)
 
 
     inference_job = hub.submit_inference_job(
