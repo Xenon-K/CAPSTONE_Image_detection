4a2fff3f2161a05e3d128e73f87d78b71489a49b
KodingKiddo <lucgomes100@gmail.com>
2025-03-19 20:52:36 -0700

    Started File for sorting

---
+++ SortRename.py
@@ -0,0 +1,7 @@
+import os
+#nowhere near finished I just copy pasted this offa stack overflow and got distracted with other work
+search_dir = "/mydir/"
+os.chdir(search_dir)
+files = filter(os.path.isfile, os.listdir(search_dir))
+files = [os.path.join(search_dir, f) for f in files] # add path to each file
+files.sort(key=lambda x: os.path.getmtime(x))
