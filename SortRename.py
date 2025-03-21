a197bc2e126d5f8acada797d0c023ad83a6a3e4c
KodingKiddo <lucgomes100@gmail.com>
2025-03-20 17:18:03 -0700

    SortRename update

--- SortRename.py
+++ SortRename.py
@@ -1,7 +1,11 @@
 import os
-#nowhere near finished I just copy pasted this offa stack overflow and got distracted with other work
-search_dir = "/mydir/"
+#gets relative filepath of each file in the directory into a list
+#what needs to be done is renaming these files with a defined format
+#need fields for Device, Model, and Runtime
+search_dir = os.getcwd()+"/toSort/"
 os.chdir(search_dir)
 files = filter(os.path.isfile, os.listdir(search_dir))
 files = [os.path.join(search_dir, f) for f in files] # add path to each file
 files.sort(key=lambda x: os.path.getmtime(x))
+
+print(files)
