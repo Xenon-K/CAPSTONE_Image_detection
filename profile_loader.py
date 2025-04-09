a3a2208194fea3670a422f4db7b47b2d8e3a076e
Kabire-A <160642553+Kabire-A@users.noreply.github.com>
2025-04-09 14:41:27 -0700

    Update profile_loader.py

--- profile_loader.py
+++ profile_loader.py
@@ -1,5 +1,9 @@
 import qai_hub as hub
-
+from qai_hub_models.utils.printing import print_profile_metrics_from_job
+from typing import Any
+from qai_hub_models.configs.perf_yaml import bytes_to_mb
+from collections import Counter
+'''
 model = hub.get_model("mnov4je9m")
 device = hub.Device("Samsung Galaxy S24 Ultra")
 
@@ -9,8 +13,8 @@ profile_job = hub.submit_profile_job(
     device=device,
 )
 assert isinstance(profile_job, hub.ProfileJob)
-
-'''profile_job = hub.get_job('jpvq26krg')
+'''
+profile_job = hub.get_job('jpvq26krg')
 
 results = profile_job.download_profile()
 #profile_job.download_results('artifacts')
@@ -18,9 +22,22 @@ results = profile_job.download_profile()
 exec_details = results['execution_detail']
 exec_summary = results['execution_summary']
 
+profile_data: dict[str, Any] = profile_job.download_profile()  # type: ignore
+print_profile_metrics_from_job(profile_job, profile_data)
+
 for keys in exec_summary:
     print(keys, ":", exec_summary[keys])
 
-#print(exec_summary.keys())
-#print(exec_summary)
-'''
+peak_memory_bytes = exec_summary["inference_memory_peak_range"]
+mem_min = bytes_to_mb(peak_memory_bytes[0])
+mem_max = bytes_to_mb(peak_memory_bytes[1])
+
+print(mem_min)
+print(mem_max)
+
+compute_unit_counts = Counter(
+        [op.get("compute_unit", "UNK") for op in profile_data["execution_detail"]]
+    )
+print(compute_unit_counts['NPU'])
+#print(exec_details.keys())
+#print(exec_details)
