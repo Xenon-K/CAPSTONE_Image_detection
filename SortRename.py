f2080939b9d46769dcc392116bcb110e3fdec5d5
Lucas <44794479+KodingKiddo@users.noreply.github.com>
2025-04-09 17:07:05 -0700

    Sort Rename proof of concept

--- SortRename.py
+++ SortRename.py
@@ -2,17 +2,22 @@ import os
 #gets relative filepath of each file in the directory into a list
 #what needs to be done is renaming these files with a defined format
 #need fields for Device, Model, and Runtime
-search_dir = os.getcwd()+"/toSort/"
-out_dir = os.getcwd()+"/sortedOut/"
-os.chdir(search_dir)
-files = filter(os.path.isfile, os.listdir(search_dir))
-files = [os.path.join(search_dir, f) for f in files] # add path to each file
-files.sort(key=lambda x: os.path.getmtime(x))
-device="D47"
+device="D47"#set up variables
 model="M10"
 runtime="tf"
 label=device+"-"+model+"-"+runtime+"-"
+
+os.makedirs(os.getcwd()+"/completed"+"/"+label+"/")#make output directory
+
+search_dir = os.getcwd()+"/toSort/"#get search directory
+out_dir = os.getcwd()+"/completed"+"/"+label+"/"#get output directory
+os.chdir(search_dir)#changes directory to search directory
+files = filter(os.path.isfile, os.listdir(search_dir))
+files = [os.path.join(search_dir, f) for f in files] # add path to each file
+files.sort(key=lambda x: os.path.getmtime(x))
+
 trace=1
+
 for file in files:
     if trace < 10:
         os.rename(file, out_dir + label + "0" + str(trace)+".h5")
