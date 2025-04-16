ff24892da6f7f6fc6a87c437e041aef344f9d918
Kabire-A <160642553+Kabire-A@users.noreply.github.com>
2025-04-16 11:39:45 -0700

    Update device_benchmark.py
    Add detr support

--- device_benchmark.py
+++ device_benchmark.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 import torch.nn.functional
 from pydash import to_lower
-from torchvision.ops import box_convert
-from torchvision.ops import nms
+from torchvision.ops import box_convert, nms
 import json
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
@@ -16,6 +15,7 @@ from typing import Any
 from qai_hub_models.configs.perf_yaml import bytes_to_mb
 from collections import Counter
 from qai_hub_models.utils.printing import print_profile_metrics_from_job
+from scipy.special import softmax
 
 detr = ['dz7z48rr9', 'dz2r53807', 'd82nd3e07', 'do7ml3v39', 'dq9kr31o7',
         'dk7gkm162', 'd67jw4gz2', 'dn7xze8r9', 'dp70n6q09', 'dv74k3ny2']
@@ -32,35 +32,36 @@ yolo_q = ['dp70nxz09','d678rmgp2','do7mlrd39','dq9kr4mo7','dr9wmzem2',
           'dz2r5qp07','dp7lgwpj2','d693m5y37','dj7d0y3p9','d82ndg6o7',
           'dv74kylz2','dz7z41jy9','dv910p3x2','dw26405g9','dx9e8nlv9']
 
+
 '''-------------------------------------'''
 # put your models to run in here
 models = [("M01", ""),
-          ("M02", ""),
+          ("M02", "mq8eo12gq"),
           ("M03", ""),
-          ("M04", ""),
+          ("M04", "mnl3pkdjn"),
           ("M05", ""),
-          ("M06", "mn7ezxyvm"),
+          ("M06", "mqpv407jq"),
           #("M07", ""),
-          ("M08", "mn1e4wx8m"),
-          ("M09", "mn4vgl6zn"),
-          ("M10", "mngw7gxem"),
-          ("M11", "mq34x676n"),
-          ("M12", "mnjl9401q"),
-          ("M15", "mqy6evlrm"),
-          ("M16", "mq8eodpjq"),
-          ("M17", "mn1e478vm")]
-Device = 'D28'
+          ("M08", "mmxxvzykm"),
+          ("M09", "mmxxvz9jm"),
+          ("M10", "mm54lwpyn"),
+          ("M11", "mq2lkzr7n"),
+          ("M12", "mq8eoxwpq"),
+          ("M15", "mqy6ewe5m"),
+          ("M16", "mmr4x2j6q"),
+          ("M17", "mnoew2z7q")
+          ]
+Device = 'D34'
 Runtime = 'tf'
-device_id = "Google Pixel 5 (Family)"
+device_id = "Samsung Galaxy S21 (Family)"
 '''-------------------------------------'''
 
 # static variable
 confidence_threshold = 0.0
 iou_threshold=0.6
-batch_size = 250
 convert_flag = True # if output is coco 80 true, if coco 91 false
 
-def process_h5(filename, confidence_threshold, category_mapping, file_no, idx_convert):
+def process_yolo(filename, confidence_threshold, category_mapping, file_no, idx_convert, batch_size):
     predictions = []
 
     with h5py.File(filename, "r") as file:
@@ -132,6 +133,117 @@ def process_h5(filename, confidence_threshold, category_mapping, file_no, idx_co
             image_id += 1
     return predictions
 
+def process_detr(filename, confidence_threshold, category_mapping, file_no, batch_size):
+    predictions = []
+
+    with h5py.File(filename, "r") as file:
+        # Get the logits and bboxes from the h5 file
+        img_count = len(file['data']['0'])
+        image_id = 0 + (batch_size * file_no)      # get index shift based on batch no for accessing image detail csv
+        for i in range(img_count):
+            # get original image id and dimensions from csv
+            image_name = image_details[image_id]['Image Name']
+            image_width = image_details[image_id]['Original Width']
+            image_height = image_details[image_id]['Original Height']
+
+            # extract data from h5 file
+            batch = 'batch_' + str(i)
+            out = torch.tensor(np.array((file['data']['0'][batch])))
+            bboxes = torch.tensor(np.array((file['data']['1'][batch])))
+            scores = torch.tensor(softmax(out.numpy(), -1))  # softmax the logits
+
+            # check each prediction
+            for j in range(len(scores[0])):
+                score = scores[0][j]
+
+                # if prediction meets confidence threshold get data and save to put in results json
+                if score.max() > confidence_threshold:
+                    pred_class = np.argsort(score, axis=0)[-1:].item()  # Get class with highest score
+                    pred_score = score.max().item()
+                    if category_mapping.get(pred_class, 0) == 0:
+                        pred_class = np.argsort(score, axis=0)[-2].item()
+                        sorted_indices = np.argsort(score, axis=0)
+                        # Get the second highest index (second to last in sorted order)
+                        second_pred_class = sorted_indices[-2].item()
+                        # Get the second highest score
+                        pred_score = score[second_pred_class].item()
+                    #print("Class ", pred_class)
+                      # Max confidence score
+                    #print("Score ", pred_score)
+                    pred_box = bboxes[0][j].tolist()  # The bounding box
+
+                    # Convert box from cxcywh format (detr) to xywh (coco)
+                    pred_box = box_convert(torch.tensor(pred_box), 'cxcywh', 'xywh').tolist()
+
+                    # After converting to xywh, scale to original dimensions
+                    pred_box[0] = round(pred_box[0] * image_width, 2)  # x1 * image_width
+                    pred_box[1] = round(pred_box[1] * image_height, 2)  # y1 * image_height
+                    pred_box[2] = round(pred_box[2] * image_width, 2)  # x2 * image_width
+                    pred_box[3] = round(pred_box[3] * image_height, 2)  # y2 * image_height
+
+
+                    # Store the result in list to store in json later
+                    predictions.append({
+                    "image_id": image_name,
+                    "category_id": category_mapping.get(pred_class, 0),
+                    "bbox": pred_box,
+                    "score": round(pred_score, 3)
+                    })
+            image_id += 1
+    return predictions
+
+def process_detr_pp(filename, confidence_threshold, category_mapping, file_no, batch_size):
+    predictions = []
+
+    with h5py.File(filename, "r") as file:
+        # Get the logits and bboxes from the h5 file
+        img_count = len(file['data']['0'])
+        image_id = 0 + (batch_size * file_no)      # get index shift based on batch no for accessing image detail csv
+        for i in range(img_count):
+            # get original image id and dimensions from csv
+            image_name = image_details[image_id]['Image Name']
+            image_width = image_details[image_id]['Original Width']
+            image_height = image_details[image_id]['Original Height']
+
+            # extract data from h5 file
+            batch = 'batch_' + str(i)
+
+            bboxes = torch.tensor(np.array((file['data']['0'][batch])))
+            scores = torch.tensor(np.array((file['data']['1'][batch])))
+            classes = torch.tensor(np.array((file['data']['2'][batch])))
+
+
+            # check each prediction
+            for j in range(len(scores[0])):
+                score = scores[0][j]
+
+                # if prediction meets confidence threshold get data and save to put in results json
+                if score.max() > confidence_threshold:
+                    pred_class=classes[0][j].item()
+                    pred_score = score.item()
+                    pred_box = bboxes[0][j].tolist()  # The bounding box
+
+                    # Convert box from cxcywh format (detr) to xywh (coco)
+                    pred_box = box_convert(torch.tensor(pred_box), 'xyxy', 'xywh').tolist()
+
+                    xscale = image_width / 480
+                    yscale = image_height / 480
+                    # After converting to xywh, scale to original dimensions
+                    pred_box[0] = round(pred_box[0] * xscale, 2)  # x1 * image_width
+                    pred_box[1] = round(pred_box[1] * yscale, 2)  # y1 * image_height
+                    pred_box[2] = round(pred_box[2] * xscale, 2)  # x2 * image_width
+                    pred_box[3] = round(pred_box[3] * yscale, 2)  # y2 * image_height
+
+                    # Store the result in list to store in json later
+                    predictions.append({
+                    "image_id": image_name,
+                    "category_id": category_mapping.get(pred_class, 0),
+                    "bbox": pred_box,
+                    "score": round(pred_score, 3)
+                    })
+            image_id += 1
+    return predictions
+
 def sort_files():
     folder_path = 'toSort'
     files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
@@ -185,8 +297,11 @@ def move_completed(data, result_name):
               os.getcwd() + "/completed" + "/" + result_name + "/" + result_name + '-results.json')
     print(f"COCO evaluation results saved to " + csv_filename)
 
+def get_model_num(modelID):
+    return int(modelID[1:])
+
 def get_dataset(modelID):
-    number = int(modelID[1:])
+    number = get_model_num(modelID)
 
     if 1 <= number <= 5:
         return detr
@@ -255,6 +370,7 @@ def run_benchmark(model_id, device_id):
 
     for jobs in job_list:
         jobs.download_results('toSort')
+
     results = profile_job.download_profile()
 
     sort_files()
@@ -282,8 +398,21 @@ def run_benchmark(model_id, device_id):
         try:
             # Read data from each h5 file and get results
             print("reading file: " + h5_file)
-            file_predictions = process_h5(h5_file_path, confidence_threshold, category_mapping, file_count,
-                                          index_convert)
+            with h5py.File(h5_file_path, "r") as file:
+                batches = len(file['data']['0'])
+                print('Batch size:', batches)
+                if 'data' in file and '2' in file['data']:
+                    detr_pp = True
+                else:
+                    detr_pp = False
+            if get_model_num(Model) > 5:
+                file_predictions = process_yolo(h5_file_path, confidence_threshold, category_mapping, file_count,
+                                            index_convert, batches)
+            else:
+                if detr_pp:
+                    file_predictions = process_detr_pp(h5_file_path, confidence_threshold, category_mapping, file_count, batches)
+                else:
+                    file_predictions = process_detr(h5_file_path, confidence_threshold, category_mapping, file_count, batches)
             all_results.extend(file_predictions)
             file_count += 1
         except Exception as e:
